f94f6c486d2708a278598b417d189b7b52c84ff1
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-13 14:39:38 +0200

    Update dvbv5.h

--- dvbv5.h
+++ dvbv5.h
@@ -33,3 +33,4 @@ dvb_open_descriptor* neutrinoNT_dvb_dev_open(dvb_device *dvb, dvb_dev_list* dvb_
 struct dvb_v5_fe_parms* neutrinoNT_dvb_dev_get_params(dvb_device* dvb_device);
 int neutrinoNT_dvb_poll(int fd, unsigned int seconds);
 int neutrinoNT_dvb_dev_dmx_set_section_filter(dvb_open_descriptor *open_dev, int pid, unsigned filtsize, MonoArray* filter,MonoArray* mask, MonoArray* mode, unsigned int flags);
+int neutrinoNT_dvb_dev_read(dvb_open_descriptor *open_dev, MonoArray* buffer, unsigned count);
