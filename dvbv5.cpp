169a9b53e9efbaf9e1d13e18ba32a1803096dd40
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-13 17:55:35 +0200

    Update dvbv5.cpp

--- dvbv5.cpp
+++ dvbv5.cpp
@@ -18,7 +18,7 @@ int neutrinoNT_dvb_eit_descriptor_get(dvb_table_eit_event* event, MonoString* na
 	    case extended_event_descriptor:
 		auto extendedEventDescriptor = (dvb_desc_event_extended*)_descriptor;
 		if (extendedEventDescriptor->text)
-		    text.mono_string_new(mono_domain_get(), extendedEventDescriptor->text);
+		    text = mono_string_new(mono_domain_get(), extendedEventDescriptor->text);
 		break;
 	    }
 	}
