6696f866ae02332f29757fd32ec5e0e63803b1af
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-13 14:10:19 +0200

    Create dvbv5.cpp

---
+++ dvbv5.cpp
@@ -0,0 +1,34 @@
+
+#include <libdvbv5/dvb-dev.h>
+
+dvb_open_descriptor* neutrinoNT_dvb_dev_open(dvb_device *dvb, dvb_dev_list* dvb_dmx, int flags)
+{
+	return dvb_dev_open(dvb, dvb_dmx->sysname, flags);
+}
+
+struct dvb_v5_fe_parms* neutrinoNT_dvb_dev_get_params(dvb_device* dvb_device)
+{
+	return dvb_device->fe_parms;
+}
+int neutrinoNT_dvb_poll(int fd, unsigned int seconds)
+{
+    fd_set set;
+    struct timeval timeout;
+    int ret;
+
+    /* Initialize the file descriptor set. */
+    FD_ZERO(&set);
+    FD_SET(fd, &set);
+
+    /* Initialize the timeout data structure. */
+    //timeout.tv_sec = milliseconds / 1000;
+    //timeout.tv_usec = (milliseconds % 1000) * 1000;
+    timeout.tv_sec = seconds;
+    timeout.tv_usec = 0;
+
+    /* `select' logfuncreturns 0 if timeout, 1 if input available, -1 if error. */
+    do ret = select(FD_SETSIZE, &set, NULL, NULL, &timeout);
+    while (ret == -1 && errno == EINTR);
+
+    return ret;
+}
