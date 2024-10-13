32fbdc4d4d20a5e1ee4833630f8660ac51cf232c
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-13 14:23:01 +0200

    Update dvbv5.cpp

--- dvbv5.cpp
+++ dvbv5.cpp
@@ -1,11 +1,19 @@
 
 #include "dvbv5.h"
 
+int neutrinoNT_dvb_dev_dmx_set_section_filter(dvb_open_descriptor *open_dev, pid, unsigned filtsize, MonoArray* filter,MonoArray* mask, MonoArray* mode, unsigned int flags = DMX_IMMEDIATE_START | DMX_CHECK_CRC)
+{
+	unsigned char* _mode = null;
+	unsigned char* _filter = mono_array_addr(filter, unsigned char, 0);
+	unsigned char* _mask = mono_array_addr(mask, unsigned char, 0);
+	if (mode != null)
+		_mode = mono_array_addr(mode, unsigned char, 0);
+	return dvb_dev_dmx_set_section_filter(open_dev, pid, filtsize, _filter, _mask, _mode, flags);
+}
 dvb_open_descriptor* neutrinoNT_dvb_dev_open(dvb_device *dvb, dvb_dev_list* dvb_dmx, int flags)
 {
 	return dvb_dev_open(dvb, dvb_dmx->sysname, flags);
 }
-
 struct dvb_v5_fe_parms* neutrinoNT_dvb_dev_get_params(dvb_device* dvb_device)
 {
 	return dvb_device->fe_parms;
@@ -29,6 +37,5 @@ int neutrinoNT_dvb_poll(int fd, unsigned int seconds)
     /* `select' logfuncreturns 0 if timeout, 1 if input available, -1 if error. */
     do ret = select(FD_SETSIZE, &set, NULL, NULL, &timeout);
     while (ret == -1 && errno == EINTR);
-
     return ret;
 }
